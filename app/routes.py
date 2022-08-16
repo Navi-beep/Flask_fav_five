f5ad1871188ba54861ae5108553e49109acd619e
Navi-Beep <estawars30@gmail.com>
2022-08-15 22:06:03 -0500

    centered my div

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,6 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    my_info = {
-        'name': 'Elise',
-        'nickname': 'Nav-beep'
-    }
+
     favorite_artists = ['Aphex Twin', 'Carpenter Brut', 'Boards of Canada', 'deadmau5', 'Joe Hisaishi']
-    return render_template('index.html',me=my_info, artists=favorite_artists)
+    return render_template('index.html', artists=favorite_artists)
