d30f4ba7aa70f6e0e8591fef4bcbe0d73acc6967
Navi-Beep <estawars30@gmail.com>
2022-08-15 23:27:36 -0500

    changed margins

--- app/routes.py
+++ app/routes.py
@@ -5,5 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
 
-    favorite_artists = ['Aphex Twin', 'Carpenter Brut', 'Boards of Canada', 'deadmau5', 'Joe Hisaishi']
-    return render_template('index.html', artists=favorite_artists)
+    favorite_artists = ['Aphex Twin', 'Carpenter Brut', 'Boards of Canada', 'deadmau5', 'Joe Hisaishi', 'Pink Floyd']
+
+    favorite_drinks = ['Iced Blonde Vanilla Latte', 'Brown Sugar Cream Cold Brew', 'Iced Mocha with cinnamon', 'Iced Matcha Vanilla Latte', 'Cold Brew']
+
+    return render_template('index.html', drinks= favorite_drinks, artists=favorite_artists)
